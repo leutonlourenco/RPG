c92c6b56e6fae20c5656b538f80551d377f95aaf
Leuton Lourenço <leuton.j@gmail.com>
2026-04-09 22:43:01 -0300

    att

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -37,7 +37,6 @@ const btn = document.getElementById('backToTop');
       window.scrollTo({ top: 0, behavior: 'smooth' });
     });
 //AÇÃO QUE IMPEDE A TELA HORIZONTAL DO MOBILE
-if (screen.orientation && screen.orientation.lock) {
-  screen.orientation.lock('portrait')
-}
+screen.orientation.lock('portrait');
+
 
