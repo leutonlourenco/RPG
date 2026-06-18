e21c607a44caaf2366426915d4e0de2441470cf7
Leuton Lourenço <leuton.j@gmail.com>
2026-06-17 23:28:42 -0300

    att

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,44 +1,30 @@
-const hamburger = document.getElementById("hamburger");
-const nav = document.getElementById("nav");
-const overlay = document.getElementById("overlay");
 
-hamburger.addEventListener("click", () => {
-  nav.classList.toggle("active");
-  overlay.classList.toggle("active");
+const btn = document.getElementById("btn");
+
+// Mostrar ou esconder o botão
+window.addEventListener("scroll", () => {
+
+    if (window.scrollY > 300) {
+
+        btn.classList.add("show");
+
+    } else {
+
+        btn.classList.remove("show");
+
+    }
+
 });
 
-overlay.addEventListener("click", () => {
-  nav.classList.remove("active");
-  overlay.classList.remove("active");
+// Voltar ao topo
+btn.addEventListener("click", () => {
+
+    window.scrollTo({
+
+        top: 0,
+        behavior: "smooth"
+
+    });
+
 });
-
-//MUDANDO A COR QUANDO O MOUSE PASSA PRO CIMA E QUANDO ELE SAI
-const mudaCor = document.querySelectorAll('.mudaCor') 
-mudaCor.forEach(cor => {
-    cor.addEventListener('mouseover', () => {
-        cor.style.background = 'rgb(255, 0, 0)'
-    })
-    cor.addEventListener('mouseout', () => {
-    cor.style.background = ''
-    })
-}); 
-
-//BOTÃO DE VOLTA PARA O TOPO
-const btn = document.getElementById('backToTop');
-    window.addEventListener('scroll', () => {
-      if (window.scrollY > 400) {
-        btn.classList.add('show');
-      } else {
-        btn.classList.remove('show');
-      }
-    });
-
-    btn.addEventListener('click', () => {
-      window.scrollTo({ top: 0, behavior: 'smooth' });
-    });
-//AÇÃO QUE IMPEDE A TELA HORIZONTAL DO MOBILE
-screen.orientation.lock('portrait');
-
-window.addEventListener("resize", mudouTamanho);
-
 
