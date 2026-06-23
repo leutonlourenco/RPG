fcf221871683790b9e1810293808e14bfd21ce98
Leuton Lourenço <leuton.j@gmail.com>
2026-06-22 22:42:56 -0300

    att

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -27,4 +27,15 @@ btn.addEventListener("click", () => {
     });
 
 });
+// MENU HAMBURGUER
+const hamburger =
+document.querySelector(".hamburger");
 
+const nav =
+document.querySelector(".nav-links");
+
+hamburger.addEventListener("click", () => {
+
+    nav.classList.toggle("active");
+
+});
